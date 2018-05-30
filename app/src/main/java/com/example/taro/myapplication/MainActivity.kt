de08af1182c905d2c0ab93c00c64d18f24ad554e
morita657 <morita657@gmail.com>
2018-05-30 11:18:25 +0900

    Declare list module

--- app/src/main/java/com/example/taro/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/taro/myapplication/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.taro.myapplication
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    internal lateinit var listview: ListView
+    internal lateinit var list:MutableList<String>
+    internal lateinit var adapter:ListAdapter
+    internal var mediaPlayer: MediaPlayer ?= null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        }
     }
 }
