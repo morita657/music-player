f9b7794a9926687d9c347777dcef4e2264b6801f
morita657 <morita657@gmail.com>
2018-05-31 16:36:15 +0900

    Replace stop method into release method to release memory

--- app/src/main/java/com/example/taro/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/taro/myapplication/MainActivity.kt
@@ -17,9 +17,10 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var list:MutableList<String>
 //    ListAdapter is used to customze list view layout
     internal lateinit var adapter:ListAdapter
-//    MediaPlayer is used to control audio files and streams
+//    MediaPlayer is used to control audio files and streams, public class
+//    ? mark is to allow to pass null value
     internal var mediaPlayer: MediaPlayer ?= null
-//Override is one of the method for class and inheritance.
+//    Override is one of the method for class and inheritance.
 //    If we want to override function, we require the explicit annotations like below
     override fun onCreate(savedInstanceState: Bundle?) {
 //    Use "super" to call parent class constructor
@@ -30,32 +31,38 @@ class MainActivity : AppCompatActivity() {
 //    findViewById is the moethod to find the view from the layout resources
 //    "as" is cast operator, and usually cast operator throws an exception if the cast is not possible.
 //    cast operator is used to convert a value to another type
+//    Get the ListView
         listview = findViewById<View>(R.id.listview) as ListView
         list = ArrayList()
 //    runtime is the state when the program gets running
 //    ::class.java gives you the Java Class<?>
 
-//!! is operator, the not-null assertion operator that converts any valu to a non-null type,
-// and throws an exception if the value is null
+//    !! is operator, the not-null assertion operator that converts any valu to a non-null type,
+//    and throws an exception if the value is null
         val fields = R.raw::class.java!!.getFields()
         for(i in fields.indices){
             list.add(fields[i].getName())
         }
+//    ArrayAdapter provides views for an ArrayView
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
         listview.adapter = adapter
-
+//    Invoke callback when an item in the list is clicked
         listview.onItemClickListener = AdapterView.OnItemClickListener{AdapterView, view,i,l ->
             if(mediaPlayer != null){
                 mediaPlayer!!.release()
             }
+//            Get id of resource
             val singh = resources.getIdentifier(list[i], "raw", packageName)
+//            Create a MediaPlayer for a given resource id.
             mediaPlayer = MediaPlayer.create(this, singh)
             mediaPlayer!!.start()
         }
     }
     fun stop(view: View){
         val myToast = Toast.makeText(this, "Stop!!", Toast.LENGTH_SHORT)
+//     Pop up "Stop!!" message
         myToast.show()
-        mediaPlayer!!.stop()
+//     Release unnecessary memory and instances of codecs being held
+        mediaPlayer!!.release()
     }
 }
