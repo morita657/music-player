c5e9a3e7a78ade1a6441e83c14d818b3b2580d2f
morita657 <morita657@gmail.com>
2018-06-24 12:25:59 +0900

    Create seekBar

--- app/src/main/java/com/example/taro/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/taro/myapplication/SecondActivity.kt
@@ -5,38 +5,90 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import android.support.annotation.IntDef
+import android.widget.ListView
+import java.lang.annotation.RetentionPolicy
+import java.util.concurrent.Executors
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.media.MediaPlayer.OnPreparedListener
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_second.*
+import android.os.Handler
 
-class SecondActivity : AppCompatActivity() {
-    //    MediaPlayer is used to control audio files and streams, public class
-//    ? mark is to allow to pass null value
+class SecondActivity : AppCompatActivity(),OnSeekBarChangeListener{
     internal var mediaPlayer: MediaPlayer ?= null
-    var mValue: Int = 0
+    internal var mValue: Int = 0
+    internal var progressView: TextView? = null
+    internal var seekbarStatusView: TextView? = null
+    internal var seekbarView: SeekBar ?= null
+    internal var runnable: Runnable ?= null
+    private val handler = Handler()
+    internal lateinit var seekbarGage: SeekBar
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val signh = intent.getIntExtra(SONG_ID,0)
+        mediaPlayer = MediaPlayer.create(this, signh)
+        progressView = this.progress
+        seekbarStatusView = this.seekbarStatus
+        seekbarView = this.seekbar
+        seekbarView!!.setOnSeekBarChangeListener(this)
+        mediaPlayer!!.setOnPreparedListener(object: MediaPlayer.OnPreparedListener{
+            override fun onPrepared(mp: MediaPlayer?) {
+                println(mediaPlayer!!.getDuration())
+                seekbarView!!.setMax(mediaPlayer!!.getDuration())
+                playCycle()
+                mediaPlayer!!.start()
+            }
+        })
     }
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int,fromUser: Boolean) {
+            mediaPlayer!!.seekTo(progress)
+    }
+    fun playCycle(){
+            runnable = object : Runnable {
+                override fun run() {
+                    seekbarView!!.setProgress(mediaPlayer!!.currentPosition)
+                    playCycle()
+                }
+            }
+            handler.postDelayed(runnable, 1000)
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar) {
+        seekbarStatusView!!.text = "Started Tracking Touch"
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
+        seekbarStatusView!!.text = "Stopped Tracking Touch"
+
+    }
+
     companion object {
         const val SONG_ID = "song_id"
     }
     fun play(view: View){
-        val signh = intent.getIntExtra(SONG_ID,0)
-        mediaPlayer = MediaPlayer.create(this, signh)
+//        val currentProgress = mediaPlayer!!.currentPosition
         mediaPlayer!!.start()
-
+        handler.post(runnable)
     }
     fun stop(view: View){
         val myToast = Toast.makeText(this, "Stop!!", Toast.LENGTH_SHORT)
 //     Pop up "Stop!!" message
         myToast.show()
-        mediaPlayer!!.release()
+        mediaPlayer!!.pause()
     }
     fun fastfoward(view:View){
         mValue += 30; //change this value to control how much to forward
         mediaPlayer!!.seekTo(mediaPlayer!!.currentPosition+mValue)
     }
     fun backward(view: View){
-        mValue -= 30; //change this value to control how much to forward
+        mValue -= 30; //change this value to control how much to backward
         mediaPlayer!!.seekTo(mediaPlayer!!.currentPosition+mValue)
     }
 }
